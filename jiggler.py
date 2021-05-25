659c11fb9e88880cd70fb77e1355d5353f166dcc
Ayush Bindlish <ayushbindlish@gmail.com>
2021-05-25 22:29:11 +0530

    added readme and jiggler.py

---
+++ jiggler.py
@@ -0,0 +1,17 @@
+# Mouse Jiggler
+#         Created By:
+#              Ayush Bindlish
+
+# Taking cursor to any of the corners of the screen will terminate the script
+
+# To RUN: python3 ./jiggler.py
+
+import pyautogui
+import time
+while True:
+    pyautogui.moveRel(0, 0)
+
+    time.sleep(2)
+
+
+
